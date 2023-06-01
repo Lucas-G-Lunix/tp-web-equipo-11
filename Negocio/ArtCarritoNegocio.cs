013e917ae3023f0b674b6cc1c957055495f84afb
Leonardo Mendoza <leoo_m@hotmail.com>
2023-06-01 12:06:26 -0300

    Se

---
+++ Negocio/ArtCarritoNegocio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class ArtCarritoNegocio
+    {
+        public List<ArtCarrito> listar()
+        {
+            List<ArtCarrito> lista = new List<ArtCarrito>();
+            return lista;
+        }
+
+    }
+}
