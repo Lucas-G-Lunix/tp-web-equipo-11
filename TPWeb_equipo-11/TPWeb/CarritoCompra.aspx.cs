2ed569400557775c5ed2e3b8a7bb5548333a1706
Leonardo Mendoza <leoo_m@hotmail.com>
2023-06-01 13:43:40 -0300

    Se agrego pagina de carrito, Se realizo el pageLoad con una gridview de los articulos que esten en la Session

---
+++ TPWeb_equipo-11/TPWeb/CarritoCompra.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
+
+namespace TPWeb
+{
+    public partial class CarritoCompra : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["ListaCarrito"] == null)
+            {
+                ArtCarritoNegocio negocio = new ArtCarritoNegocio();
+                Session.Add("ListaCarrito", negocio.listar());
+
+            }
+
+
+            dgvArticulos.DataSource = Session["ListaCarrito"];
+            dgvArticulos.DataBind();
+
+            List<ArtCarrito> listaSession = new List<ArtCarrito>();
+            listaSession = (List<ArtCarrito>)Session["ListaCarrito"];
+            decimal total = 0;
+            lblPrecio.Text = "TOTAL: $" + total;
+        }
+
+
+        protected void dgvArticulos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+     
+
+        }
+    }
+}
--- TPWeb_equipo-11/TPWeb/CarritoCompra.aspx.designer.cs
+++ TPWeb_equipo-11/TPWeb/CarritoCompra.aspx.designer.cs
@@ -11,25 +11,25 @@ namespace TPWeb
 {
 
 
-    public partial class Master
+    public partial class CarritoCompra
     {
 
         /// <summary>
-        /// Control form1.
+        /// Control dgvArticulos.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.GridView dgvArticulos;
 
         /// <summary>
-        /// Control ContentPlaceHolder1.
+        /// Control lblPrecio.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.Label lblPrecio;
     }
 }
